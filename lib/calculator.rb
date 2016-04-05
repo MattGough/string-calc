227fcff149f7f7754f2eb26c47f5a08d829b1e41
Matt <mattpgough@gmail.com>
2016-04-05 11:53:01 +0100

    Added test for summing two numbers within a string, and refactored code to several private methods.

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,7 +1,25 @@
 class Calculator
 
   def int_add(numbers)
-    numbers == "" ? 0 : numbers.to_i
+    empty?(numbers) ? 0 : sum(numbers)
+  end
+
+  private
+
+  def sum(numbers)
+    to_array(numbers).inject(:+)
+  end
+
+  def to_array(numbers)
+    split(numbers).map { |number| number.to_i }
+  end
+
+  def split(numbers)
+    numbers.split(',')
+  end
+
+  def empty?(numbers)
+    numbers == ""
   end
 
 end
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -1,7 +1,7 @@
 require './lib/calculator'
 
 describe Calculator do
-  subject(:calculator) { described_class.new}
+  subject(:calculator) { described_class.new }
 
   describe '#int_add' do
 
@@ -12,6 +12,10 @@ describe Calculator do
     it 'should return integer version of single input string' do
       random_number = rand(10)
       expect(calculator.int_add(random_number.to_s)).to eq(random_number)
+    end
+
+    it 'should return the sum of two number passed in a string' do
+      expect(calculator.int_add("4,5")).to eq(9)
     end
   end
 end
